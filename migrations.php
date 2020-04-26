6a58f961162238e27dfa64bc7da77625bacc6064
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2020-04-26 17:13:51 +0200

    Fix small migration issues

--- migrations.php
+++ migrations.php
@@ -7,7 +7,7 @@ return [
     'column_name' => 'version',
     'column_length' => 14,
     'executed_at_column_name' => 'executed_at',
-    'migrations_directory' => '/src/Database/Core/src/Migrations',
+    'migrations_directory' => '/src/Database/Core/src/Migration',
     'all_or_nothing' => true,
     'check_database_platform' => true,
 ];
--- src/App/src/Service/Factory/ToDoServiceFactory.php
+++ src/App/src/Service/Factory/ToDoServiceFactory.php
@@ -10,7 +10,7 @@ use Psr\Container\ContainerInterface;
 
 class ToDoServiceFactory
 {
-    public function __invoke(ContainerInterface $container)
+    public function __invoke(ContainerInterface $container): ToDoService
     {
         return new ToDoService($container->get(ToDoRepository::class));
     }
